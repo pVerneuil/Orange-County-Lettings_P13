474b7f5ece99df28477524691d55e096b7ee5cbe
verneuilp <verneuil.paul@gmail.com>
2022-10-06 15:11:35 +0200

    linting

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", '127.0.0.1',".herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", '127.0.0.1', ".herokuapp.com"]
 
 
 # Application definition
